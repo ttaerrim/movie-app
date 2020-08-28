423e5d445caba9e8eeae1973625910e57e7d3d61
Lee <ltr0121@naver.com>
2020-08-28 21:01:53 +0900

    컴포넌트 전달하는 방법 <props 사용하기>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Potato from './Potato';
 
+function Food({ fav }) {
+  return <h3>I like {fav}</h3>;
+}
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="삼겹살" />
+      <Food fav="쭈꾸미" />
     </div>
 
   );
